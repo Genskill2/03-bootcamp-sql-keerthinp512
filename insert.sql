94d1c636dd87d2e3b7ec8bd1633530725529d269
Keerthi NP <keerthimohan512@gmail.com>
2021-06-19 13:13:26 +0530

    "Added all files"

--- insert.sql
+++ insert.sql
@@ -11,36 +11,36 @@ INSERT INTO books (title,publisher) VALUES ('The UNIX Programming Environment',1
 INSERT INTO books (title,publisher) VALUES ('Cryptonomicon',2);
 INSERT INTO books (title,publisher) VALUES ('Deep Work',3);
 INSERT INTO books (title,publisher) VALUES ('Atomic Habits',4);
-INSERT INTO books (title,publisher) VALUES ('The City and the City',5);
+INSERT INTO books (title,publisher) VALUES ('The City and The City',5);
 INSERT INTO books (title,publisher) VALUES ('The Great War for Civilisation',6);
 
 INSERT INTO subjects(name) VALUES('C');
 INSERT INTO subjects(name) VALUES('UNIX');
 INSERT INTO subjects(name) VALUES('Technology');
-INSERT INTO subjects(name) VALUES('Go');
 INSERT INTO subjects(name) VALUES('Science Fiction');
 INSERT INTO subjects(name) VALUES('Productivity');
 INSERT INTO subjects(name) VALUES('Psychology');
 INSERT INTO subjects(name) VALUES('Politics');
 INSERT INTO subjects(name) VALUES('History');
+INSERT INTO subjects(name) VALUES('Go');
 
 INSERT INTO books_subjects (book,subject) VALUES ( 1,1);
 INSERT INTO books_subjects (book,subject) VALUES ( 1,2);
 INSERT INTO books_subjects (book,subject) VALUES ( 1,3);
-INSERT INTO books_subjects (book,subject) VALUES ( 2,4);
+INSERT INTO books_subjects (book,subject) VALUES ( 2,9);
 INSERT INTO books_subjects (book,subject) VALUES ( 2,3);
 INSERT INTO books_subjects (book,subject) VALUES ( 3,2);
 INSERT INTO books_subjects (book,subject) VALUES ( 3,3);
 INSERT INTO books_subjects (book,subject) VALUES ( 4,3);
-INSERT INTO books_subjects (book,subject) VALUES ( 4,5);
+INSERT INTO books_subjects (book,subject) VALUES ( 4,4);
 INSERT INTO books_subjects (book,subject) VALUES ( 5,3);
-INSERT INTO books_subjects (book,subject) VALUES ( 5,6);
+INSERT INTO books_subjects (book,subject) VALUES ( 5,5);
+INSERT INTO books_subjects (book,subject) VALUES ( 6,5);
 INSERT INTO books_subjects (book,subject) VALUES ( 6,6);
-INSERT INTO books_subjects (book,subject) VALUES ( 6,7);
-INSERT INTO books_subjects (book,subject) VALUES ( 7,5);
-INSERT INTO books_subjects (book,subject) VALUES ( 7,8);
+INSERT INTO books_subjects (book,subject) VALUES ( 7,4);
+INSERT INTO books_subjects (book,subject) VALUES ( 7,7);
+INSERT INTO books_subjects (book,subject) VALUES ( 8,7);
 INSERT INTO books_subjects (book,subject) VALUES ( 8,8);
-INSERT INTO books_subjects (book,subject) VALUES ( 8,9);
 
 
 
